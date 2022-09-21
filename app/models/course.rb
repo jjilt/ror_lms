bbf98549b85b5f7e1f73778d5a7c0f3ba7ec5430
Joy Nichols <joynichols@joys-air.lan>
2022-09-21 18:08:49 -0400

    enrollment back end done

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
-
+  has_many :enrollments, dependent: :destroy
+  has_many :users, through: :enrollments
+  
   validates :title, :desc, :ctype, presence: true
 end
